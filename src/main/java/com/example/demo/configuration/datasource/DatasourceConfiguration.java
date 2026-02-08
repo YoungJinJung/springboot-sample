63d60dddb238193f5b0a34a854e3e528cb348e23
kuma.jung <yj.jung5078@gmail.com>
2026-02-08 17:31:03 +0900

    comment unused part

--- src/main/java/com/example/demo/configuration/datasource/DatasourceConfiguration.java
+++ src/main/java/com/example/demo/configuration/datasource/DatasourceConfiguration.java
@@ -1,131 +1,131 @@
-package com.example.demo.configuration.datasource;
+// package com.example.demo.configuration.datasource;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.*;
-import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.rds.RdsClient;
-import software.amazon.awssdk.services.rds.RdsUtilities;
-import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
-import software.amazon.awssdk.services.rds.model.RdsException;
+// import com.zaxxer.hikari.HikariDataSource;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.beans.factory.annotation.Qualifier;
+// import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+// import org.springframework.boot.context.properties.ConfigurationProperties;
+// import org.springframework.context.annotation.*;
+// import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+// import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+// import software.amazon.awssdk.regions.Region;
+// import software.amazon.awssdk.services.rds.RdsClient;
+// import software.amazon.awssdk.services.rds.RdsUtilities;
+// import software.amazon.awssdk.services.rds.model.GenerateAuthenticationTokenRequest;
+// import software.amazon.awssdk.services.rds.model.RdsException;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
+// import javax.sql.DataSource;
+// import java.util.HashMap;
+// import java.util.Map;
 
-@Configuration
-@Profile("!local")
-public class DatasourceConfiguration {
-    private static RdsClient rdsClient = RdsClient.builder().region(Region.AP_NORTHEAST_2).build();
-    private static final Logger log = LoggerFactory.getLogger(DatasourceConfiguration.class);
+// @Configuration
+// @Profile("!local")
+// public class DatasourceConfiguration {
+//     private static RdsClient rdsClient = RdsClient.builder().region(Region.AP_NORTHEAST_2).build();
+//     private static final Logger log = LoggerFactory.getLogger(DatasourceConfiguration.class);
 
-    @Bean(name = "writerDataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.writer")
-    public DataSourceProperties writerDataSourceProperties(){
-        return new DataSourceProperties();
-    }
-    @Bean(name = "writerDataSource")
-    public DataSource writerDataSource(@Qualifier("writerDataSourceProperties") DataSourceProperties writerDataSourceProperties) {
-        String writerUsername = writerDataSourceProperties.getUsername();
-        String writerUrl = writerDataSourceProperties.getUrl();
+//     @Bean(name = "writerDataSourceProperties")
+//     @ConfigurationProperties(prefix = "spring.datasource.writer")
+//     public DataSourceProperties writerDataSourceProperties(){
+//         return new DataSourceProperties();
+//     }
+//     @Bean(name = "writerDataSource")
+//     public DataSource writerDataSource(@Qualifier("writerDataSourceProperties") DataSourceProperties writerDataSourceProperties) {
+//         String writerUsername = writerDataSourceProperties.getUsername();
+//         String writerUrl = writerDataSourceProperties.getUrl();
 
-        log.info("Url: {}, UserName: {}",writerUrl,writerUsername);
+//         log.info("Url: {}, UserName: {}",writerUrl,writerUsername);
 
-        String writerAuthToken = getAuthToken(rdsClient, writerUrl, writerUsername);
+//         String writerAuthToken = getAuthToken(rdsClient, writerUrl, writerUsername);
 
-        writerDataSourceProperties.setPassword(writerAuthToken);
+//         writerDataSourceProperties.setPassword(writerAuthToken);
 
-        return writerDataSourceProperties
-                .initializeDataSourceBuilder()
-                .type(HikariDataSource.class)
-                .build();
-    }
+//         return writerDataSourceProperties
+//                 .initializeDataSourceBuilder()
+//                 .type(HikariDataSource.class)
+//                 .build();
+//     }
 
-    @Bean(name = "readerDataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource.reader")
-    public DataSourceProperties readerDataSourceProperties(){
-        return new DataSourceProperties();
-    }
-    @Bean(name = "readerDataSource")
-    public DataSource readerDataSource(@Qualifier("readerDataSourceProperties") DataSourceProperties readerDataSourceProperties) {
-        String readerUsername = readerDataSourceProperties.getUsername();
-        String readerUrl = readerDataSourceProperties.getUrl();
+//     @Bean(name = "readerDataSourceProperties")
+//     @ConfigurationProperties(prefix = "spring.datasource.reader")
+//     public DataSourceProperties readerDataSourceProperties(){
+//         return new DataSourceProperties();
+//     }
+//     @Bean(name = "readerDataSource")
+//     public DataSource readerDataSource(@Qualifier("readerDataSourceProperties") DataSourceProperties readerDataSourceProperties) {
+//         String readerUsername = readerDataSourceProperties.getUsername();
+//         String readerUrl = readerDataSourceProperties.getUrl();
 
-        log.info("Url: {}, UserName: {}",readerUrl,readerUsername);
+//         log.info("Url: {}, UserName: {}",readerUrl,readerUsername);
 
-        String readerAuthToken = getAuthToken(rdsClient, readerUrl, readerUsername);
+//         String readerAuthToken = getAuthToken(rdsClient, readerUrl, readerUsername);
 
-        readerDataSourceProperties.setPassword(readerAuthToken);
+//         readerDataSourceProperties.setPassword(readerAuthToken);
 
-        return readerDataSourceProperties
-                .initializeDataSourceBuilder()
-                .type(HikariDataSource.class)
-                .build();
-    }
+//         return readerDataSourceProperties
+//                 .initializeDataSourceBuilder()
+//                 .type(HikariDataSource.class)
+//                 .build();
+//     }
 
-    @Bean
-    @DependsOn({"writerDataSource", "readerDataSource"})
-    public DataSource routingDataSource(
-            @Qualifier("writerDataSource") DataSource writerDataSource,
-            @Qualifier("readerDataSource") DataSource readerDataSource) {
+//     @Bean
+//     @DependsOn({"writerDataSource", "readerDataSource"})
+//     public DataSource routingDataSource(
+//             @Qualifier("writerDataSource") DataSource writerDataSource,
+//             @Qualifier("readerDataSource") DataSource readerDataSource) {
 
-        RoutingDataSource routingDataSource = new RoutingDataSource();
+//         RoutingDataSource routingDataSource = new RoutingDataSource();
 
-        Map<Object, Object> targetDataSources = new HashMap<>();
-        targetDataSources.put(RoutingDataSource.DataSourceType.WRITER, writerDataSource);
-        targetDataSources.put(RoutingDataSource.DataSourceType.READER, readerDataSource);
+//         Map<Object, Object> targetDataSources = new HashMap<>();
+//         targetDataSources.put(RoutingDataSource.DataSourceType.WRITER, writerDataSource);
+//         targetDataSources.put(RoutingDataSource.DataSourceType.READER, readerDataSource);
 
-        routingDataSource.setTargetDataSources(targetDataSources);
-        routingDataSource.setDefaultTargetDataSource(writerDataSource);
+//         routingDataSource.setTargetDataSources(targetDataSources);
+//         routingDataSource.setDefaultTargetDataSource(writerDataSource);
 
-        return routingDataSource;
-    }
+//         return routingDataSource;
+//     }
 
-    @Bean("dataSource")
-    @Primary
-    @DependsOn({"routingDataSource"})
-    public DataSource dataSource() {
-        return new LazyConnectionDataSourceProxy(routingDataSource(writerDataSource(writerDataSourceProperties()), readerDataSource(readerDataSourceProperties())));
-    }
+//     @Bean("dataSource")
+//     @Primary
+//     @DependsOn({"routingDataSource"})
+//     public DataSource dataSource() {
+//         return new LazyConnectionDataSourceProxy(routingDataSource(writerDataSource(writerDataSourceProperties()), readerDataSource(readerDataSourceProperties())));
+//     }
 
-    private String[] extractHostAndPort(String jdbcUrl) {
-        String pattern = "://([^/]+)";
-        java.util.regex.Pattern p = java.util.regex.Pattern.compile(pattern);
-        java.util.regex.Matcher m = p.matcher(jdbcUrl);
+//     private String[] extractHostAndPort(String jdbcUrl) {
+//         String pattern = "://([^/]+)";
+//         java.util.regex.Pattern p = java.util.regex.Pattern.compile(pattern);
+//         java.util.regex.Matcher m = p.matcher(jdbcUrl);
 
-        if (m.find()) {
-            String hostPort = m.group(1);
-            String[] parts = hostPort.split(":");
-            return new String[]{parts[0], parts[1]};
-        }
-        return null;
-    }
+//         if (m.find()) {
+//             String hostPort = m.group(1);
+//             String[] parts = hostPort.split(":");
+//             return new String[]{parts[0], parts[1]};
+//         }
+//         return null;
+//     }
 
-    private String getAuthToken(RdsClient rdsClient, String url, String username) {
+//     private String getAuthToken(RdsClient rdsClient, String url, String username) {
 
-        RdsUtilities utilities = rdsClient.utilities();
-        String[] endpoint = extractHostAndPort(url);
+//         RdsUtilities utilities = rdsClient.utilities();
+//         String[] endpoint = extractHostAndPort(url);
 
-        try {
-            GenerateAuthenticationTokenRequest tokenRequest = GenerateAuthenticationTokenRequest.builder()
-                    .credentialsProvider(DefaultCredentialsProvider.builder().build())
-                    .username(username)
-                    .port(Integer.parseInt(endpoint[1]))
-                    .hostname(endpoint[0])
-                    .region(Region.AP_NORTHEAST_2)
-                    .build();
+//         try {
+//             GenerateAuthenticationTokenRequest tokenRequest = GenerateAuthenticationTokenRequest.builder()
+//                     .credentialsProvider(DefaultCredentialsProvider.builder().build())
+//                     .username(username)
+//                     .port(Integer.parseInt(endpoint[1]))
+//                     .hostname(endpoint[0])
+//                     .region(Region.AP_NORTHEAST_2)
+//                     .build();
 
-            return utilities.generateAuthenticationToken(tokenRequest);
+//             return utilities.generateAuthenticationToken(tokenRequest);
 
-        } catch (RdsException e) {
-            System.out.println(e.getLocalizedMessage());
-        }
-        return "";
-    }
-}
+//         } catch (RdsException e) {
+//             System.out.println(e.getLocalizedMessage());
+//         }
+//         return "";
+//     }
+// }
--- src/main/java/com/example/demo/configuration/datasource/RoutingDataSource.java
+++ src/main/java/com/example/demo/configuration/datasource/RoutingDataSource.java
@@ -1,31 +1,31 @@
-package com.example.demo.configuration.datasource;
+// package com.example.demo.configuration.datasource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+// import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-public class RoutingDataSource extends AbstractRoutingDataSource {
-    private static final Logger log = LoggerFactory.getLogger(RoutingDataSource.class);
+// public class RoutingDataSource extends AbstractRoutingDataSource {
+//     private static final Logger log = LoggerFactory.getLogger(RoutingDataSource.class);
 
-    @Override
-    protected Object determineCurrentLookupKey() {
+//     @Override
+//     protected Object determineCurrentLookupKey() {
 
-        boolean isTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
-        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
-        String currentTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
-        log.info("isTransactionActive : {}, isReadOnly : {}, currentTransactionName : {}", isTransactionActive, isReadOnly, currentTransactionName);
+//         boolean isTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+//         boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+//         String currentTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
+//         log.info("isTransactionActive : {}, isReadOnly : {}, currentTransactionName : {}", isTransactionActive, isReadOnly, currentTransactionName);
 
-        if (!isTransactionActive) {
-            return DataSourceType.READER;
-        }
+//         if (!isTransactionActive) {
+//             return DataSourceType.READER;
+//         }
 
-        // 트랜잭션이 있는 경우 읽기 전용 여부에 따라 결정
-        return isReadOnly
-                ? DataSourceType.READER : DataSourceType.WRITER;
-    }
+//         // 트랜잭션이 있는 경우 읽기 전용 여부에 따라 결정
+//         return isReadOnly
+//                 ? DataSourceType.READER : DataSourceType.WRITER;
+//     }
 
-    public enum DataSourceType {
-        WRITER, READER
-    }
-}
+//     public enum DataSourceType {
+//         WRITER, READER
+//     }
+// }
